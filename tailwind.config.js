6368bf1ef9e04c84858e2d59af263a5b77929e88
Abu Syeed Abdullah <72955464+asapial@users.noreply.github.com>
2025-02-01 00:06:39 +0600

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
-
-tailwind.config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      keyframes: {
-        fadeInUp: {
-          '0%': { opacity: '0', transform: 'translateY(30px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
-        },
-      },
-      animation: {
-        'fade-in-up': 'fadeInUp 1s ease-out forwards',
-      }
-    }
-  }
+    extend: {},
+  },
+  plugins: [],
 }
-
